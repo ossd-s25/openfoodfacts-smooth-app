185033e154d6c96acb201c115b7f7eda4c655571
Andy Lin <andy.lin.2004@outlook.com>
2025-04-28 13:21:02 -0400

    adding state control to report image screen

--- packages/smooth_app/lib/pages/nutripatrol/report_image.dart
+++ packages/smooth_app/lib/pages/nutripatrol/report_image.dart
@@ -6,15 +6,21 @@ import 'package:smooth_app/widgets/smooth_list_diff.dart';
 import 'package:smooth_app/widgets/smooth_scaffold.dart';
 import 'package:smooth_app/resources/app_icons.dart' as icons;
 
-class ReportImage extends StatelessWidget {
+enum ReportReason { photoNotMatching, inappropriatePhoto, other }
+
+class ReportImageState extends State<ReportImage> {
+  ReportReason? reportReason;
+
   @override
   Widget build(BuildContext context) {
     return SmoothScaffold(
         appBar: SmoothAppBar(
           title: Text("Report an Image"),
         ),
-        body: Padding(padding: const EdgeInsets.all(20), child: Column(
-          children: [
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
               const SmoothCardWithRoundedHeader(
                 title: "Image to report",
                 leading: icons.Flag(),
@@ -27,33 +33,57 @@ class ReportImage extends StatelessWidget {
                 leading: const Text("1"),
                 child: Column(
                   children: [
-                    SmoothButtonWithArrow(text: "Photo does not match product", onTap: () => {
-                      print("Pressed 1")
-                    }),
-                    SmoothButtonWithArrow(text: "Photo is inappropriate", onTap: () => {
-                      print("Pressed 2")
-                    }),
-                    SmoothButtonWithArrow(text: "Other reason", onTap: () => {
-                      print("Pressed 3")
-                    }),
+                    SmoothButtonWithArrow(
+                        text: "Photo does not match product",
+                        onTap: () => {
+                              setState(() {
+                                reportReason = ReportReason.photoNotMatching;
+                              })
+                            }),
+                    SmoothButtonWithArrow(
+                        text: "Photo is inappropriate",
+                        onTap: () => {
+                              setState(() {
+                                reportReason = ReportReason.inappropriatePhoto;
+                              })
+                            }),
+                    SmoothButtonWithArrow(
+                        text: "Other reason",
+                        onTap: () => {
+                              setState(() {
+                                reportReason = ReportReason.other;
+                              })
+                            }),
                   ],
                 ),
               ),
-              const SmoothCardWithRoundedHeader(
-                title: "Explanation",
-                leading: Text("2"),
-                child: Row(
-                  children: [Text("A"), Text("B")],
+              if (reportReason == ReportReason.photoNotMatching)
+                const SmoothCardWithRoundedHeader(
+                  title: "Explanation",
+                  leading: Text("2"),
+                  child: Row(
+                    children: [const Text("A"), const Text("B")],
+                  ),
                 ),
-              ),
-              const SmoothCardWithRoundedHeader(
+              SmoothCardWithRoundedHeader(
                 title: "Comment",
-                leading: Text("3"),
-                child: Row(
+                leading: Text(
+                    reportReason == ReportReason.photoNotMatching ? "3" : "2"),
+                child: const Row(
                   children: [Text("A"), Text("B")],
                 ),
               ),
             ],
-        ),));
+          ),
+        ));
+  }
+}
+
+class ReportImage extends StatefulWidget {
+  const ReportImage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ReportImageState();
   }
 }
