231f40a39fdc5ec12a6d73bcb383d7a01d70ee31
Andy Lin <andy.lin.2004@outlook.com>
2025-05-01 18:17:04 -0400

    working text box

--- packages/smooth_app/lib/pages/nutripatrol/report_image.dart
+++ packages/smooth_app/lib/pages/nutripatrol/report_image.dart
@@ -1,25 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:smooth_app/cards/product_cards/smooth_product_image.dart';
 import 'package:smooth_app/database/transient_file.dart';
 import 'package:smooth_app/generic_lib/buttons/smooth_button_with_arrow.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
 import 'package:smooth_app/generic_lib/widgets/smooth_card.dart';
+import 'package:smooth_app/pages/preferences/user_preferences_dev_mode.dart';
 import 'package:smooth_app/resources/app_icons.dart' as icons;
 import 'package:smooth_app/widgets/smooth_app_bar.dart';
 import 'package:smooth_app/widgets/smooth_scaffold.dart';
 
-enum ReportReason { 
-  photoNotMatching, photoNotMatchingAndContinuingWithReport, inappropriatePhoto, other 
+enum ReportReason {
+  photoNotMatching,
+  photoNotMatchingAndContinuingWithReport,
+  inappropriatePhoto,
+  other
 }
 
 class ReportImageState extends State<ReportImage> {
   ReportReason? reportReason;
   String reportExplanation = '';
+  final textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
     final TransientFile transientFile = _getTransientFile(
       widget.product,
       widget.imageField,
@@ -32,46 +39,50 @@ class ReportImageState extends State<ReportImage> {
         body: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
+            spacing: 12,
             children: [
               SmoothCardWithRoundedHeader(
-                title: 'Image to report',
-                leading: const icons.Flag(),
-                child: Padding(padding: const EdgeInsets.all(12.0), child: Row(spacing: 12.0,
-                  children: [
-                    ProductPicture.fromTransientFile(
-                      product: widget.product,
-                      imageField: widget.imageField,
-                      language: widget.language,
-                      allowAlternativeLanguage: false,
-                      transientFile: transientFile,
-                      size: const Size(50, 50),
-                      onTap: null,
-                      errorTextStyle: const TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      heroTag: ProductPicture.generateHeroTag(
-                        widget.product.barcode!,
-                        widget.imageField,
-                      ),
-                      showObsoleteIcon: false,
-                      showOwnerIcon: true,
-                    ),
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  title: 'Image to report',
+                  leading: const icons.Flag(),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      spacing: 12.0,
                       children: [
-                        Text(
-                          'Image to Report',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ProductPicture.fromTransientFile(
+                          product: widget.product,
+                          imageField: widget.imageField,
+                          language: widget.language,
+                          allowAlternativeLanguage: false,
+                          transientFile: transientFile,
+                          size: const Size(50, 50),
+                          onTap: null,
+                          errorTextStyle: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          heroTag: ProductPicture.generateHeroTag(
+                            widget.product.barcode!,
+                            widget.imageField,
+                          ),
+                          showObsoleteIcon: false,
+                          showOwnerIcon: true,
                         ),
-                        Text('Contributor: xxxxx'),
-                        Text('Date uploaded: 01/01/2000'),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Image to Report',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            const Text('Contributor: xxxxx'),
+                            const Text('Date uploaded: 01/01/2000'),
+                            Text(reportExplanation),
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
-                )
-              ),
+                  )),
               SmoothCardWithRoundedHeader(
                 title: 'Reason',
                 leading: const Text('1'),
@@ -101,44 +112,80 @@ class ReportImageState extends State<ReportImage> {
                   ],
                 ),
               ),
-              if (reportReason == ReportReason.photoNotMatching || reportReason == ReportReason.photoNotMatchingAndContinuingWithReport)
+              if (reportReason == ReportReason.photoNotMatching ||
+                  reportReason ==
+                      ReportReason.photoNotMatchingAndContinuingWithReport)
                 SmoothCardWithRoundedHeader(
                   title: 'Explanation',
                   leading: const Text('2'),
-                  child: Column(
-                    children: <Widget>[
-                      const Text(
-                          'Open Food Facts is a user maintained database with over 3 million products (according to Wikipedia).'),
-                      const Text(
-                          'If you own this product, you can take a photo of it to correct the product details.'),
-                      SmoothButtonWithArrow(
-                          text: 'I would like to take a picture of the product',
-                          onTap: () => {print('To be implemented...')}),
-                      SmoothButtonWithArrow(text: 'I would like to continue making a report', onTap: () => {
-                        setState(() {
-                          reportReason = ReportReason.photoNotMatchingAndContinuingWithReport;
-                        })
-                      })
-                    ],
+                  child: Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: ListView(
+                      children: <Widget>[
+                        const Text(
+                            'Open Food Facts is a user maintained database with over 3 million products (according to Wikipedia).'),
+                        const Text(
+                            'If you own this product, you can take a photo of it to correct the product details.'),
+                        SmoothButtonWithArrow(
+                            text:
+                                'I would like to take a picture of the product',
+                            onTap: () => {print('To be implemented...')}),
+                        SmoothButtonWithArrow(
+                            text: 'I would like to continue making a report',
+                            onTap: () => {
+                                  setState(() {
+                                    reportReason = ReportReason
+                                        .photoNotMatchingAndContinuingWithReport;
+                                  })
+                                })
+                      ],
+                    ),
                   ),
                 ),
-              if (reportReason != null && reportReason != ReportReason.photoNotMatching)
+              if (reportReason != null &&
+                  reportReason != ReportReason.photoNotMatching)
                 SmoothCardWithRoundedHeader(
                   title: 'Comment',
-                  leading: Text(reportReason == ReportReason.photoNotMatching
+                  leading: Text(reportReason ==
+                          ReportReason.photoNotMatchingAndContinuingWithReport
                       ? '3'
                       : '2'),
-                  child: Row(
-                    children: [
-                      // TextFormField(
-                      //   onSaved: (String? newValue) => {
-                      //     setState(() {
-                      //       reportExplanation = newValue ?? reportExplanation;
-                      //     })
-                      //   },
-                      // ),
-                      Text(reportExplanation)
-                    ],
+                  child: TextFormField(
+                    minLines: null,
+                    maxLines: null,
+                    textInputAction: TextInputAction.newline,
+                    textCapitalization: TextCapitalization.sentences,
+                    // spellCheckConfiguration: (prefs.getFlag(
+                    //                 UserPreferencesDevMode
+                    //                     .userPreferencesFlagSpellCheckerOnOcr) ??
+                    //             false) &&
+                    //         (Platform.isAndroid || Platform.isIOS)
+                    //     ? const SpellCheckConfiguration()
+                    //     : const SpellCheckConfiguration.disabled(),
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: LARGE_SPACE,
+                        vertical: SMALL_SPACE,
+                      ),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: ROUNDED_BORDER_RADIUS,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: ROUNDED_BORDER_RADIUS,
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                          width: 5.0,
+                        ),
+                      ),
+                    ),
+                    onTapOutside: (_) =>
+                        FocusManager.instance.primaryFocus?.unfocus(),
+                    onChanged: (String value) {
+                      setState(() {
+                        reportExplanation = value;
+                      });
+                    },
                   ),
                 ),
             ],
@@ -150,17 +197,19 @@ class ReportImageState extends State<ReportImage> {
     final Product product,
     final ImageField imageField,
   ) =>
-  TransientFile.fromProduct(
-    product,
-    imageField,
-    widget.language,
-  );
+      TransientFile.fromProduct(
+        product,
+        imageField,
+        widget.language,
+      );
 }
 
 class ReportImage extends StatefulWidget {
   const ReportImage(
-      {
-        required this.language, required this.imageField, required this.product, super.key});
+      {required this.language,
+      required this.imageField,
+      required this.product,
+      super.key});
 
   final OpenFoodFactsLanguage language;
   final ImageField imageField;
